ee14ca790a5a092fdbbabc809093bfc9fff6c643
Joshua Rose <joshua.rose@outlook.com>
2018-07-20 09:03:25 +0100

    Minify JS, split CSS into a separate file

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,23 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-    entry: ['./src/index'],
+    entry: ["./src/index"],
 
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'MWO Bags',
+            title: "MWO Bags",
             inject: false,
-            template: require('html-webpack-template'),
+            template: require("html-webpack-template"),
             headHtmlSnippet: '<meta name="viewport" content="width=device-width, initial-scale=1">'
         }),
         new webpack.NamedModulesPlugin()
     ],
 
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
     },
 
     resolve: {
@@ -26,11 +26,10 @@ module.exports = {
 
     module: {
         rules: [
-
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             {
                 test: /\.tsx?$/,
-                use: ['awesome-typescript-loader']
+                use: ["awesome-typescript-loader"]
             },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
@@ -38,18 +37,12 @@ module.exports = {
 
             {
                 test: /\.(sc|sa|c)ss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    "sass-loader"
-                ]
+                use: ["style-loader", "css-loader", "sass-loader"]
             },
 
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
+                use: ["file-loader"]
             }
         ]
     }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,36 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge.smart(common, {
+    mode: "production",
 
-    mode: 'production',
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false
+            }),
+            new OptimizeCssAssetsPlugin({})
+        ]
+    },
 
     plugins: [
-        new OptimizeCssAssetsPlugin({})
-    ]
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(sc|sa|c)ss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            }
+        ]
+    }
 });
